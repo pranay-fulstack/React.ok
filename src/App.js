978c9fd95fa2d52e6415875cf28bc7c0c0e68951
pranay-fulstack <jyothipranay0437@gmail.com>
2021-03-16 22:19:28 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
- import React from 'react'
- import Pranay1 from "./Components/Pranay1";
+ import React from 'react';
+ import ContactApp from './AxiosContactApp/ContactApp';
+
+/*  import Pranay1 from "./Components/Pranay1";
  import {Pranay2,Pranay3 }from "./Components/Pranay2";
 
- import Counter from "./Components/Counter";
+ import Counter from "./Components/Counter"; */
  function App () {
-     return (
-         <div>
-             <h1>Hello Welcome pvp.</h1>,
-             <p>Hello Welcom to Bnglr Market.</p>
-             <Pranay1/>
-             <Pranay2/>
-             <Pranay3/>
+     return <>
+             <h1>Hello Welcome pvp.</h1>
+             <ContactApp />
             
-             <Counter/>
-         </div>    
+         </>   
          
-     );
+    
  };
  
  export default App;
---
+++ src/AxiosContactApp/ContactApp.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import ContactList from './ContactList';
+import ContactCard from './ContactCard';
+import Axios from 'axios';
+
+class ContactApp extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            contacts: null,
+            selectedContact: null,
+            errorMessage: null,
+        }
+    }
+    pullData=(contact)=>{
+        this.setState({
+            selectedContact: contact,
+        })
+    }
+    componentDidMount(){
+        let URL="https://gist.githubusercontent.com/narasimhareddyprostack/7e344f346f47bc53a889d78b5258d0c9/raw/56d531cb936d9c79e2417e5d0e5d8c9c876800f2/contactlist";
+        Axios.get(URL)
+        .then((response)=>{
+            this.setState({
+                contacts: response.data,
+            })
+        })
+        .catch((err)=>{
+            this.setState({
+                errorMessage: err,
+            })
+        })
+    }
+    render() {
+        return<>
+        <h1>ContactApp component</h1>
+        <div className="container">
+        <div className="row">
+        <div className="col">
+        <p className="h4">
+        Your all-in-one online address book for iOS, Android, web, Mac
+        OS, & Gmail. 
+        </p>
+        </div>
+        </div>
+        </div>
+        <div className="container">
+        <div className="row">
+        <div className="col-md-8">
+        <ContactList contactData={this.state.contacts} pullData={this.pullData} />
+        </div>
+        <div className="col-md-4">
+        <ContactCard selectedContact={this.state.selectedContact} />
+        </div>
+        </div>
+        </div>
+        
+        
+        </>
+    }
+}
+
+export default ContactApp;
